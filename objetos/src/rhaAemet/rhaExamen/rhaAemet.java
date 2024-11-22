fbc66aaa034a17546f12f30d130690194d1fdcfd
RosaHaro <rosaharoalarcon@hotmail.es>
2024-11-22 10:38:23 +0100

    10:30

---
+++ objetos/src/rhaAemet/rhaExamen/rhaAemet.java
@@ -0,0 +1,37 @@
+package rhaAemet.rhaExamen;
+
+public class rhaAemet{
+    static final int rhaNCIUDADES = 8;
+    static String[] rhanombres = {"Almería", "Granada", "Jaén", "Málaga", "Sevilla" , "Cádiz", "Córdoba", "Sevilla" };
+    public static rhaAemet[]rha = new rhaAemet[rhaNCIUDADES];
+   
+
+
+    
+public static void main (String[]args){
+    
+    rhaAemet.rhaGenerarRegistroTemp();
+    //rhaAemet.rhaListarRegistroTemp();
+}
+public static void rhaGenerarRegistroTemp(){
+ //Es la entrada de datos, se toma cada nombre del array de nombres, se piden valorMax y valorMin y se crea cada RegCiudad
+
+ for (int i=0; i<rhaAemet.rhaNCIUDADES; i++){
+    System.out.print("La ciudad " + (i+1) + ": ");
+    rhaAemet.rhanombres[i] = rhaAemet.entrada.next();
+
+    }
+
+}
+public static void rhaListarRegistroTemp(){
+//Se muestra el listado de de las ciudades con sus temperaturas semanales 
+
+
+}
+
+}
+
+   
+
+
+
---
+++ objetos/src/rhaAemet/rhaExamen/rhaRegCiudad.java
@@ -0,0 +1,21 @@
+package rhaAemet.rhaExamen;
+
+import java.util.Scanner;
+
+public class rhaRegCiudad {
+    public static Scanner entrada = new Scanner(System.in);
+    public final int rhaNDIAS = 7;
+    public int tmax =0;
+    static int tmin =0;
+
+    
+
+    //**Constructor: se le pasa siempre el nombre de la ciudad y el valor de la tmax y tmin. En caso de que no se pasen al constructor estas serán 40º y -1º. 
+//se llamará a los métodos de asignación y 
+//se llama al método generarTemperaturas que genera dinámicamente las temperaturas de cada día de la semana, entre vmax y vim,  y almacenarlas en el array temperaturas. 
+//métodos getters y setters
+//mostrarTabla*//
+
+}
+
+    
